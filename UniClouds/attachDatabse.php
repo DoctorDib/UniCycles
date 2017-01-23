f45160279e5f2cf36df366ba9eec42403c1bb633
MattGuest0317 <up766290@myport.ac.uk>
2017-01-23 20:36:56 +0000

    Started on PHP
    a few php changes and documents added, as well as a few fixes to the current html including table names

---
+++ UniClouds/attachDatabse.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matt Guest
+ * Date: 23/01/2017
+ * Time: 19:56
+ */
+
+function connectDatabase()
+{
+    $servername = "unicycles.ddns.net:157";
+    $username = "root";
+    $password = "unicycles";
+    $databaseName = "localhost";
+    $datain = $_POST["data"];
+
+// Create database Connection
+    $Connection = new mysqli($servername, $username, $password, $databaseName);
+// Check Connection
+    if ($Connection->connect_error) {
+        die("Connection Failed:" . $Connection->connect_error);
+    }
+}
+function simpleCall($number){
+    return $number;
+}
+/**
+ * @param $location
+ */
+function freeSpaces($location){
+    connectDatabase();
+    $sql = "SELECT id, firstname, lastname FROM MyGuests";
+}
+?>
